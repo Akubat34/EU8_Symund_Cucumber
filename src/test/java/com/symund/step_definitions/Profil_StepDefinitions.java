34821d6126df6135c194e03d7ebd3252b92cf612
mehme <mehmet.tokatli999@gmail.com>
2022-07-22 15:26:00 +0300

    profile snippets created

---
+++ src/test/java/com/symund/step_definitions/Profil_StepDefinitions.java
@@ -0,0 +1,46 @@
+package com.symund.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Profil_StepDefinitions {
+    @Given("navigate to login page")
+    public void navigate_to_login_page() {
+
+    }
+    @When("click on the profile icon")
+    public void click_on_the_profile_icon() {
+
+    }
+    @When("click the setting button")
+    public void click_the_setting_button() {
+
+    }
+    @Then("verify user is on the profile setting page")
+    public void verify_user_is_on_the_profile_setting_page() {
+
+    }
+    @When("change the name in the {string} box")
+    public void change_the_name_in_the_box(String string) {
+
+    }
+    @Then("verify user has changed the {string}")
+    public void verify_user_has_changed_the(String string) {
+
+    }
+
+    @When("make {string} info as private")
+    public void make_info_as_private(String string) {
+
+    }
+    @Then("verify that {string} information can be made private")
+    public void verify_that_information_can_be_made_private(String string) {
+
+    }
+    @Then("Verify that the current local time appears under the Local dropdown menu")
+    public void verify_that_the_current_local_time_appears_under_the_local_dropdown_menu() {
+
+    }
+
+}
