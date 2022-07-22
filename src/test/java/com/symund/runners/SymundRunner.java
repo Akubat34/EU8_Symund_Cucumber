824befc007969dcc0e0b5042d6158afd2faead66
sertyertan <sertyertan@gmail.com>
2022-07-22 16:51:43 +0300

    Files Module Add to Favorites TC completed

--- src/test/java/com/symund/runners/SymundRunner.java
+++ src/test/java/com/symund/runners/SymundRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/symund/step_definitions",
         dryRun = false,
-        tags = "@1"
+        tags = "@UND-1244"
 )
 public class SymundRunner {
 }
