69f86b25c07cfda5ab46d40e75f8310675498526
ugur <ugurtanir3455@gmail.com>
2022-07-24 21:40:43 +0300

    working on step definitions

--- src/test/java/com/symund/page/TasksPage.java
+++ src/test/java/com/symund/page/TasksPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.FindBy;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TasksPage extends BasePage{
+public class TasksPage extends BasePage {
 
 
     @FindBy(xpath = "//div[@class='app-navigation-entry-icon sprt-add']")
@@ -18,46 +18,66 @@ public class TasksPage extends BasePage{
     @FindBy(id = "newListInput")
     public WebElement addListPlaceholder;
 
-    @FindBy(xpath ="//*[@id=\"app-navigation-vue\"]/ul/li[7]/ul/div/form/input[3]")
+    @FindBy(xpath = "//*[@id=\"app-navigation-vue\"]/ul/li[7]/ul/div/form/input[3]")
     public WebElement saveBtn;
 
+    @FindBy(xpath = "//span[@class='icon icon-sprt-bw sprt-task-star']")
+    public WebElement star;
+
+    @FindBy(xpath = "//label[@class='reactive no-nav']")
+    public WebElement checkBox;
+
+    @FindBy(xpath = "//span[@title='Important']")
+    public WebElement importantTaskBar;
+
+    @FindBy(id = "target")
+    public WebElement taskPlaceHolder;
+
+    @FindBy(xpath = "//span[@title='Completed']")
+    public WebElement completedTaskBar;
+
+    public boolean checkTask(String name) {
+        String locator = "//div[div='" + name + "']";
+        return Driver.getDriver().findElement(By.xpath(locator)).getText().equalsIgnoreCase(name);
+    }
+
     /**
-     * finds element in task list
+     * clicks the checkbox to do it completed
+     * @param name
+     */
+    public void clickTaskCheckBox(String name) {
+        String locator = "//div[*='" + name + "']/../div/label";
+        Driver.getDriver().findElement(By.xpath(locator)).click();
+    }
+
+    /**
+     * To check task can be checked completed
+     *
      * @param name
      * @return
      */
-    public WebElement findTasksList(String name){
-        String locator ="//span[@title='"+ name +"']";
-        return Driver.getDriver().findElement(By.xpath(locator));
+    public boolean checkCompletedTask(String name) {
+        boolean result = false;
+        List<WebElement> elements = Driver.getDriver().findElements(By.xpath("//ol[@collection-id='completed']"));
+
+        for (WebElement element : elements) {
+            if (element.getText().equalsIgnoreCase(name)) {
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        return result;
     }
 
     /**
-     * deletes list
-     * @param name
+     * check the task is it checked or not to do it completed
+     * @param newTaskName name that you enter
+     * @return  task list include your task list
      */
-
-    public void deleteList(String name){
-
-
-        String locator ="//span[@title='"+ name +"']";
-        WebElement element = Driver.getDriver().findElement(By.xpath(locator));
-
-        element.click();
-        String locator1 ="//span[@title='" + name + "']/../../div/div/div";
-        WebElement deleteElement =Driver.getDriver().findElement(By.xpath(locator1));
-        deleteElement.click();
-
-        String deleteLocator ="//li[@class='action has-tooltip']";
-        Driver.getDriver().findElement(By.xpath(deleteLocator)).click();
-        BrowserUtils.waitFor(10);
-
-
-    }
-
-    public boolean checkTasksName (String newTaskName){
-       List<WebElement> tasks = Driver.getDriver().findElements(By.xpath("//ul[@class='app-navigation__list']/div/li/a/span"));
-       boolean isPresent=true;
-       List<String > textOfTasks = new ArrayList<>();
+    public boolean checkTasksList(String newTaskName) {
+        List<WebElement> tasks = Driver.getDriver().findElements(By.xpath("//ul[@class='app-navigation__list']/div/li/a/span"));
+        List<String> textOfTasks = new ArrayList<>();
         for (WebElement each : tasks) {
             textOfTasks.add(each.getAttribute("title"));
         }
@@ -66,54 +86,75 @@ public class TasksPage extends BasePage{
 
     }
 
-    @FindBy(xpath = "//button[@class='inline task-star reactive no-nav']/span")
-    public WebElement star;
+    /**
+     * deletes list
+     *
+     * @param name tas list name
+     */
+    public void deleteList(String name) {
 
-    @FindBy(xpath = "//label[@class='reactive no-nav']")
-    public WebElement checkBox;
 
-    @FindBy(xpath = "//div[@class='grouped-tasks']//ol[1]//li")
-    public WebElement checked;
+        String locator = "//span[@title='" + name + "']";
+        WebElement element = Driver.getDriver().findElement(By.xpath(locator));
 
-    @FindBy(id = "target")
-    public WebElement taskPlaceHolder;
+        element.click();
+        String locator1 = "//span[@title='" + name + "']/../../div/div/div";
+        WebElement deleteElement = Driver.getDriver().findElement(By.xpath(locator1));
+        deleteElement.click();
 
-    @FindBy(xpath = "//span[@title='Completed']")
-    public WebElement completedTaskBar;
+        String deleteLocator = "//li[@class='action has-tooltip']";
+        Driver.getDriver().findElement(By.xpath(deleteLocator)).click();
+        BrowserUtils.waitFor(10);
 
-    public boolean checkTask (String name){
-        String locator ="//div[div='" + name + "']";
-        return Driver.getDriver().findElement(By.xpath(locator)).getText().equalsIgnoreCase(name);
-    }
 
-    public void clickTaskCheckBox(String name){
-        String locator = "//div[*='" + name + "']/../div/label";
-        Driver.getDriver().findElement(By.xpath(locator)).click();
     }
 
     /**
-     * To check task can be checked completed
+     * finds element in task list
      * @param name
      * @return
      */
-    public boolean checkCompletedTask(String name){
-        boolean result = false;
-        List<WebElement> elements = Driver.getDriver().findElements(By.xpath("//ol[@collection-id='completed']"));
+    public WebElement findTasksList(String name) {
+        String locator = "//span[@title='" + name + "']";
+        return Driver.getDriver().findElement(By.xpath(locator));
+    }
 
-        for (WebElement element : elements) {
-            if (element.getText().equalsIgnoreCase(name)){
-                result = true;
-            }else {
-                result= false;
+    /**
+     * checks the task that you named is it important or not
+     * @param name Task name
+     * @return Task is checked or not
+     */
+    public boolean checkTaskIsImportant(String name){
+        List<WebElement> element =Driver.getDriver().findElements(By.xpath("//div[@class='task-info']"));
+        boolean result=false;
+        for (WebElement webElement : element) {
+            if (webElement.getText().equalsIgnoreCase(name)){
+                result=true;
             }
         }
         return result;
     }
 
+    public List<String> checksTheNumberOfCurrentTask(){
+        String locator ="(//div[@class='app-navigation-entry__counter'])";
+        int number = Driver.getDriver().findElements(By.xpath(locator)).size();
+        System.out.println(number);
+        List<WebElement> webElements = new ArrayList<>();
+        List<String> element =new ArrayList<>();
+
+        for (int i = 7; i <= number; i++) {
+            locator ="(//div[@class='app-navigation-entry__counter'])" + "[" + i +"]";
+            webElements.add(Driver.getDriver().findElement(By.xpath(locator)));
+        }
+        System.out.println(webElements.size());
+        for (WebElement webElement : webElements) {
+            element.add(webElement.getText());
+        }
 
 
 
-
+        return element;
+    }
 
 
 }
