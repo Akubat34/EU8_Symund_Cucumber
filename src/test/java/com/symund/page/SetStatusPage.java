6149349561ecaddc3962701899e85cea1c53c33c
bsahinb <97312028+bsahinb@users.noreply.github.com>
2022-07-25 14:44:33 +0300

    SetStatusPage

--- src/test/java/com/symund/page/SetStatusPage.java
+++ src/test/java/com/symund/page/SetStatusPage.java
@@ -14,5 +14,13 @@ public class SetStatusPage extends BasePage{
     @FindBy(xpath = "//label[@class='user-status-online-select__label icon-user-status-dnd']")
     public WebElement doNotDisturbBtn;
 
+    @FindBy(xpath = "//label[@class='user-status-online-select__label icon-user-status-invisible']")
+    public WebElement invisibleButton;
+
+    @FindBy(xpath = "//form[@class='custom-input__form']")
+    public WebElement statusMessageBox;
+
+
+
 
 }
