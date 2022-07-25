de0f2fe7873eadb9d3f6aca28990bb01ccb294e3
bsahinb <97312028+bsahinb@users.noreply.github.com>
2022-07-25 15:58:59 +0300

    SetStatusPage

--- src/test/java/com/symund/page/SetStatusPage.java
+++ src/test/java/com/symund/page/SetStatusPage.java
@@ -24,6 +24,24 @@ public class SetStatusPage extends BasePage{
     public WebElement emojiButton;
 
 
+    //default status message
+    @FindBy(xpath = "//*[@id=\"body-user\"]/div[6]/div[2]/div/div/div[5]/div[1]/span[2]")
+    public WebElement inMeeting;
+
+    @FindBy(xpath = "//*[@id=\"body-user\"]/div[6]/div[2]/div/div/div[5]/div[2]/span[2]")
+    public WebElement commuting;
+
+    @FindBy(xpath = "//*[@id=\"body-user\"]/div[6]/div[2]/div/div/div[5]/div[3]/span[2]")
+    public WebElement workingRemotely;
+
+    @FindBy(xpath = "//*[@id=\"body-user\"]/div[6]/div[2]/div/div/div[5]/div[4]/span[2]")
+    public WebElement outSick;
+
+    @FindBy(xpath = "//*[@id=\"body-user\"]/div[6]/div[2]/div/div/div[5]/div[5]/span[2]")
+    public WebElement vacationing;
+
+    @FindBy(xpath = "//button[@class='status-buttons__primary primary']")
+    public WebElement setStatusMessageBtn;
 
 
 }
