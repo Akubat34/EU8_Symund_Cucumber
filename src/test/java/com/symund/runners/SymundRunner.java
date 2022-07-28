4376b4a194184159daf37c88aea53b24c52a7ef7
32ahmet-S <ahmetsungu198989@gmail.com>
2022-07-28 15:08:49 +0300

    Delete module is completed

--- src/test/java/com/symund/runners/SymundRunner.java
+++ src/test/java/com/symund/runners/SymundRunner.java
@@ -16,11 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/symund/step_definitions",
         dryRun = false,
-
-        tags = "@5_isNotOk"
-
-
-
+        tags = "@UND-1238"
 )
 public class SymundRunner {
 }
