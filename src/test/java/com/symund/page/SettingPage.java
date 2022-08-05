00960eefb63289fd508be8b0e3d142f8387c9a84
mehme <mehmet.tokatli999@gmail.com>
2022-08-05 15:03:49 +0300

    laters commit

--- src/test/java/com/symund/page/SettingPage.java
+++ src/test/java/com/symund/page/SettingPage.java
@@ -11,47 +11,53 @@ import java.util.List;
 public class SettingPage extends BasePage {
 
 
-            @FindBy(xpath = "//input[@id='displayname']")
-            public WebElement fullName;
+    @FindBy(xpath = "//input[@id='displayname']")
+    public WebElement fullName;
 
-            @FindBy(xpath = "//div[@id='localeexample']")
-            public WebElement localeTime;
+    @FindBy(xpath = "//form[@id='phoneform']//span")
+    public WebElement phoneNumber;
 
-            @FindBy(xpath = "//span[@id='localeexample-date']")
-            public WebElement localDate;
+    @FindBy(xpath = "//a[@data-action='v2-private']")
+    public WebElement privateClick;
+
+    @FindBy(xpath = "//div[@id='localeexample']")
+    public WebElement localeTime;
+
+    @FindBy(xpath = "//span[@id='localeexample-date']")
+    public WebElement localDate;
 
 
-            @FindBy(xpath = "//li[@data-id='settings']//a")
-            public WebElement settings;
+    @FindBy(xpath = "//li[@data-id='settings']//a")
+    public WebElement settings;
 
-            @FindBy(xpath = "//form[@id='phoneform']//div[@class='federation-menu']//span")
-            public WebElement iconOfVisibility;
+    @FindBy(xpath = "//form[@id='phoneform']//div[@class='federation-menu']//span")
+    public WebElement iconOfVisibility;
 
-            @FindBy(xpath = "//h3//label")
-            public List<WebElement> personelInfoTitle;
+    @FindBy(xpath = "//h3//label")
+    public List<WebElement> personelInfoTitle;
 
-            @FindBy(xpath = "//span[@class='user-status-menu-item__header']")
-            public WebElement userName;
+    @FindBy(xpath = "//span[@class='user-status-menu-item__header']")
+    public WebElement userName;
 
-            /**
-             * returns me the formatted date info matching with actual date
-             *
-             * @return
-             */
-            public String getCurrentLocalDate() {
+    /**
+     * returns me the formatted date info matching with actual date
+     *
+     * @return
+     */
+    public String getCurrentLocalDate() {
 
-                LocalDate localDate = LocalDate.now();
-                System.out.println(localDate.toString());
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate.toString());
 
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-                String expectedLocalDate = localDate.format(dtf);
-                System.out.println("After formatting: " + expectedLocalDate);
+        String expectedLocalDate = localDate.format(dtf);
+        System.out.println("After formatting: " + expectedLocalDate);
 
-                return expectedLocalDate;
+        return expectedLocalDate;
 
-            }
-        }
+    }
+}
 
 
 
--- src/test/java/com/symund/step_definitions/Profil_StepDefinitions.java
+++ src/test/java/com/symund/step_definitions/Profil_StepDefinitions.java
@@ -1,7 +1,6 @@
 package com.symund.step_definitions;
 
 import com.symund.page.DashBoardPage;
-import com.symund.page.LoginPage;
 import com.symund.page.SettingPage;
 import com.symund.utilities.BrowserUtils;
 import com.symund.utilities.Driver;
@@ -9,69 +8,50 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.apache.poi.ss.formula.functions.T;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
-import javax.swing.text.DateFormatter;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Profil_StepDefinitions {
-
-
-
-    SettingPage settingPage =new SettingPage();
+    SettingPage settingPage = new SettingPage();
     DashBoardPage dashboardPage = new DashBoardPage();
 
     @Given("click on the profile icon")
     public void click_on_the_profile_icon() {
-
         dashboardPage.userBtn.click();
     }
+
     @When("click the setting button")
     public void click_the_setting_button() {
-
         settingPage.settings.click();
     }
+
     @Then("verify user is on the profile setting page")
     public void verify_user_is_on_the_profile_setting_page() {
-      String expectedTitle = "Settings - Symund - QA";
-      Assert.assertEquals(expectedTitle,Driver.getDriver().getTitle());
+        String expectedTitle = "Settings - Symund - QA";
+        Assert.assertEquals(expectedTitle, Driver.getDriver().getTitle());
     }
 
     @And("change the name in the Full name box with {string}")
     public void changeTheNameInTheFullNameBoxWith(String expectedFullname) {
-
         settingPage.fullName.clear();
         settingPage.fullName.sendKeys(expectedFullname);
         BrowserUtils.waitFor(7);
+ }
 
-
-
-    }
     @Then("verify user has changed the {string}")
     public void verify_user_has_changed_the(String expectedFullname) {
-
         settingPage.navigateTo("Files");
         BrowserUtils.waitFor(3);
         dashboardPage.userBtn.click();
-
         Assert.assertEquals(expectedFullname, settingPage.userName.getText());
-
     }
-
 
     @Then("User can see the following from the personal information headings")
     public void user_can_see_the_following_from_the_personal_information_headings(List<String> expectedTitles) {
         List<String> actualTitles = BrowserUtils.getElementsText(settingPage.personelInfoTitle);
         Assert.assertTrue(actualTitles.containsAll(expectedTitles));
-
-
-        }
-
+    }
 
     @When("make {string} info as private")
     public void make_info_as_private(String string) {
@@ -79,7 +59,6 @@ public class Profil_StepDefinitions {
         settingPage.phoneNumber.click();
         BrowserUtils.waitFor(5);
         settingPage.privateClick.click();
-
     }
     @Then("verify that {string} information can be made private")
     public void verify_that_information_can_be_made_private(String string) {
@@ -87,11 +66,12 @@ public class Profil_StepDefinitions {
         Assert.assertTrue(classOfIcon.contains("icon-phone"));
 
     }
+
     @Then("Verify that the current local time appears under the Local dropdown menu")
     public void verify_that_the_current_local_time_appears_under_the_local_dropdown_menu() {
         BrowserUtils.waitForVisibility(settingPage.localDate, 5);
         Assert.assertTrue(settingPage.localeTime.isDisplayed());
-        Assert.assertEquals(settingPage.getCurrentLocalDate(),settingPage.localDate.getText());
+        Assert.assertEquals(settingPage.getCurrentLocalDate(), settingPage.localDate.getText());
 
     }
 
